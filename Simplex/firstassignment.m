1eeb03882da3699ae8e7ac6d1f8168bbb450ad38
ekiMGlz <mike.glz.96@gmail.com>
2019-04-23 19:19:26 -0500

    fix a blands rule

--- Simplex/firstassignment.m
+++ Simplex/firstassignment.m
@@ -28,20 +28,34 @@
 % sbfs = [0, 0, 1, 3, 2];
 
 % Unbounded Example (Matousek, pg 61)
-A = [1, -1, 1, 0;
-      -1, 1, 0, 1];
+% A = [1, -1, 1, 0;
+%       -1, 1, 0, 1];
+% 
+% b = [1;
+%      2];
+%
+% c = [1;
+%      0;
+%      0;     
+%      0];
+%
+% sbasis = [3, 4];
+% sbfs = [0, 0, 1, 2];
 
-b = [1;
+% Degenerate Example (Matousek, pg 62)
+A = [-1, 1, 1, 0;
+      1, 0, 0, 1];
+
+b = [0;
      2];
 
-c = [1;
-     0;
+c = [0;
+     1;
      0;     
      0];
 
 sbasis = [3, 4];
-sbfs = [0, 0, 1, 2];
-
+sbfs = [0, 0, 0, 2];
 
 [bound, obasis, obfs oval] = phaseTwo(A, b, c, sbasis, sbfs);
 
@@ -66,7 +80,7 @@ function[bound, obasis, obfs, oval] = phaseTwo(A, b, c, sbasis, sbfs)
     % bound = 0 if the problem is unbounded (there is no optimal solution)
     % bound = 1 if the problem is bounded (there is an optimal solution)
     % obasis = a vector of size m of indices of column vectors which gives an
-    % optimal feasible basis for the problem if the problem is bounded
+    %          optimal feasible basis for the problem if the problem is bounded
     % obfs = a vector of size n which is the optimal basic feasible solution corresponding to this optimal basis if the problem is bounded
     % oval = the objective value of this optimal basic feasible solution (if the problem is bounded)
 
@@ -94,35 +108,36 @@ function[bound, obasis, obfs, oval] = phaseTwo(A, b, c, sbasis, sbfs)
     T(m+1, 1) = dot(c(basic_vars), sbfs(basic_vars));
     T(m+1, 2:(n-m+1)) = transpose(c(null_vars) + transpose(Q)*c(basic_vars));
     
-    T
-
-    [ub, op, deg, candidates] = tableauStatus(T);
     
-    % Check if the problem is unbounded
-    if ub
-        bound = 0;
+    % Check the tableau
+    [bound, optimal, degenerate, candidates] = tableauStatus(T);
+    % TODO: pretty print tableau
+    printTableau(T, basic_vars, null_vars);
+    candidates
+    
+    % If the tableau shows a possible unbounded soln, exit
+    if ~bound
         obasis = [];
         obfs = [];
         oval = -inf;
         return
     end
     
-    while ~op
+    % Loop until an optimal solution is reached
+    while ~optimal
         
-        % TODO: add steepest edge step if non degenerate bfs
-        [T, enter, leave] = BlandsRuleStep(T, candidates);
-        aux = null_vars(enter-1);
+        % TODO: add different steps if non degenerate bfs
+        % Take a step using Bland's Rule
+        [T, basic_vars, null_vars] = BlandStep(T, basic_vars, null_vars, candidates);
         
-        null_vars(enter-1) = basic_vars(leave);
-        basic_vars(leave) = aux;
+        % Check the tableau
+        [bound, optimal, degenerate, candidates] = tableauStatus(T);
         % TODO: make a pretty print for tableaus
-        T
-        basic_vars
-        null_vars
-        [ub, op, deg, candidates] = tableauStatus(T);
+        printTableau(T, basic_vars, null_vars);
+        candidates
         
-        if ub
-            bound = 0;
+        % If the tableau shows a possible unbounded soln, exit
+        if ~bound
             obasis = [];
             obfs = [];
             oval = -inf;
@@ -130,7 +145,7 @@ function[bound, obasis, obfs, oval] = phaseTwo(A, b, c, sbasis, sbfs)
         end
     end
     
-    bound = 1;
+    % Get the correct values for the optimal basis, bfs and value
     obasis = sort(basic_vars);
     obfs = zeros(1, n);
     for i = 1:m
@@ -146,19 +161,21 @@ end
 %%%                                                                          %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [unbounded, optimal, degenerate, candidates] = tableauStatus(T)
+function [bound, optimal, degenerate, candidates] = tableauStatus(T)
     % Status of the tableau
     % Input:
-    % T - tableau to check
+    %   T - tableau to check
     % Output:
-    % unbounded - 1 if unbounded, else 0
-    % optimal - 1 if optimal, else 0
-    % degenerate - 1 if degenerate bfs, else 0
-    % candidates - (k)x(3) matrix, where k is the number of possible
-    % enetring varialbles for the simplex. Each row contains, in order, the
-    % index of the entering variable and the index of the leaving variable 
+    %   bounded - 1 if bounded, else 0
+    %   optimal - 1 if optimal, else 0
+    %   degenerate - 1 if degenerate bfs, else 0
+    %   candidates - (k)x(3) matrix, where k is the number of possible
+    %                enetring varialbles for the simplex. Each row contains, in order, the
+    %                index of the column representing the entering variable, 
+    %                the index of the row representing the leaving variable,
+    %                and the new value for the entering variable.
     [m, n] = size(T);
-    unbounded = 0;
+    bound = 1;
     optimal = 1;
     degenerate = 0;
     candidates = [];
@@ -177,10 +194,10 @@ function [unbounded, optimal, degenerate, candidates] = tableauStatus(T)
                 end
             end
             
-            candidates = [candidates; j, min_index];
+            candidates = [candidates; j, min_index, min];
             
             if min_index == -1
-                unbounded = 1;
+                bound = 0;
                 break;
             end
         end
@@ -198,24 +215,27 @@ end
 function [T] = nextTableau(T, col, row)
     % Calculates the next tableau
     % Input:
-    % T - (m)x(n) matrix with the previous tableau, arranged as stated in
-    %     phaseTwo
-    % col - index of the row representing the entering variable, must be a value in [2, n]
-    % row - index of the row representing the leaving variable, value in [1, m-1]
+    %   T - (m)x(n) matrix with the previous tableau, arranged as stated in
+    %       phaseTwo
+    %   col - index of the row representing the entering variable, 
+    %         must be a value in [2, n]
+    %   row - index of the row representing the leaving variable, 
+    %         value in [1, m-1]
     % Output:
-    % T - The next tableau
+    %   T - The next tableau
     % 
     
     [m, n] = size(T);
     % Save the previous column
     aux_col = T(:, col);
     
-    %Replace the entering column with [0, 0, ..., -1, 0, ..., 0] (-1 in leaving row)
+    % Replace the entering column with [0, 0, ..., -1, 0, ..., 0] (-1 in leaving row)
     T(:, col) = zeros(m, 1);
     T(row, col) = -1;
     
-    %Divide the leaving row by the coeficient of the entering variable.
+    % Divide the leaving row by the coeficient of the entering variable.
     T(row, :) = -T(row, :)/aux_col(row);
+    % Adjust the rest of the rows to the new variable
     for i = 1:m
         if i ~= row
             T(i, :) = T(i, :) + aux_col(i)*T(row, :);
@@ -224,11 +244,51 @@ function [T] = nextTableau(T, col, row)
     
 end
 
-function[T, enter, leave] = BlandsRuleStep(T, candidates)
-    % TODO: check if valid, otherwise check for min index (might need more args)
+function[T, basic_vars, null_vars] = BlandStep(T, basic_vars, null_vars, candidates)
+    % Takes a step using Bland's Rule
+    % Input:
+    %   T - (m)x(n) matrix with the previous tableau, arranged as stated in
+    %       phaseTwo
+    %   basic_vars - index of the row representing the entering variable, 
+    %                must be a value in [2, n]
+    %   null_vars - index of the row representing the leaving variable, 
+    %               value in [1, m-1]
+    %   candidates - (k)x(3) matrix containing possible candidates for 
+    %                entering the basis
+    % Output:
+    %   T - The next tableau
+    %   basic_vars - vector with the index for the basic variables
+    %   null_vars - vector with the index for the non basic variables
+
+    % Decide on the entering var
+    % Since the columns of the tableau are not necesarilly sorted with respect 
+    % to the index of each variable, the minimum index must be manually found
     enter = candidates(1, 1);
     leave = candidates(1, 2);
+    min = basic_vars(enter-1);
+    for r = transpose(candidates(2:end, :))
+        if basic_vars(r(1)-1) < min
+            min = basic_vars(r(1)-1);
+            enter = r(1);
+            leave = r(2);
+        end
+    end
+    
+    % Generate the next tableau
     T = nextTableau(T, enter, leave);
+    
+    % Swap the entering and leaving variables in basic_vars and null_vars
+    aux = null_vars(enter-1);
+    null_vars(enter-1) = basic_vars(leave);
+    basic_vars(leave) = aux;
 end
 
-
+function[] = printTableau(T, basic_vars, null_vars)
+    disp(["x\t   ", num2str(null_vars)])
+    
+    [m, n] = size(T);
+    for i = 1:m-1
+        disp([num2str(basic_vars(i)), "=\t", num2str(T(i, :))])
+    end
+    disp(["z=\t", num2str(T(m, :))])
+end
